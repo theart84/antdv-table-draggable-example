eb76f771ad7621e28d0bf269059d89cc1fb58817
theart84 <artshadow74@gmail.com>
2022-03-27 14:39:43 +0500

    feat: add draggable body table

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-layout class="layout">
+    <a-layout-header>
+      <h2 style="color: white">Ant Design Vue 3 Draggable Table Example</h2>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        <a-table
+          :columns="columns"
+          :components="components"
+        />
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script lang="ts" setup>
+import DraggableBodyWrapper from './components/DraggableBodyWrapper.vue';
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Website',
+    dataIndex: 'website',
+    key: 'website',
+  },
+];
+
+const components = {
+  body: {
+    wrapper: DraggableBodyWrapper,
+  },
+};
+</script>
---
+++ src/components/DraggableBodyWrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <draggable tag="tbody"
+             :list="data"
+             name="custom-table"
+             @change="onChange"
+             item-key="id"
+             @start="dragging = true"
+             @end="dragging = false">
+    <template #item="{ element }">
+      <tr class="ant-table-row ant-table-row-level-0">
+        <td class="ant-table-cell">{{ element.id }}</td>
+        <td class="ant-table-cell">{{ element.name }}</td>
+        <td class="ant-table-cell">{{ element.email }}</td>
+        <td class="ant-table-cell">{{ element.website }}</td>
+      </tr>
+    </template>
+  </draggable>
+</template>
+
+<script lang="ts" setup>
+import Draggable from 'vuedraggable';
+import { ref } from 'vue';
+import users from '../mock';
+
+const data = ref(users);
+
+const dragging = ref(false);
+
+const onChange = (evt: any) => {
+  console.log(evt.moved);
+  console.log(JSON.parse(JSON.stringify(data.value)));
+};
+
+</script>
